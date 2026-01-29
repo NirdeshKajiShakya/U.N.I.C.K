80c9f805e2e790809d2d7108095e87fd4a6a8e5c
Chandani <karojna30@gmail.com>
2026-01-30 01:27:23 +0545

    changes in LoginInstrumentedTest

--- app/src/androidTest/java/com/example/unick/ui/tests/LoginInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/unick/ui/tests/LoginInstrumentedTest.kt
@@ -1,15 +1,15 @@
 package com.example.unick
 
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.unick.view.AdminDashboardActivity
 import com.example.unick.view.DashboardActivity
+import com.example.unick.view.SchoolDashboard
 import com.example.unick.view.UserLoginActivity
 import org.junit.After
 import org.junit.Before
@@ -23,7 +23,6 @@ class LoginInstrumentedTest {
     @get:Rule
     val composeRule = createAndroidComposeRule<UserLoginActivity>()
 
-
     @Before
     fun setup() {
         Intents.init()
@@ -34,42 +33,30 @@ class LoginInstrumentedTest {
         Intents.release()
     }
 
+    // Test for User login
     @Test
-    fun testSuccessfulLogin_navigatesToDashboard() {
-        // Type email
-        composeRule.onNodeWithTag("email")
-            .performTextInput("ram@gmail.com")
-
-        // Type password
-        composeRule.onNodeWithTag("password")
-            .performTextInput("password")
-
-        // Click login
-        composeRule.onNodeWithTag("login")
-            .performClick()
-
-        // Verify navigation
+    fun loginUser_navigatesToUserDashboard() {
+        composeRule.onNodeWithTag("email").performTextInput("user@test.com")
+        composeRule.onNodeWithTag("password").performTextInput("user123")
+        composeRule.onNodeWithTag("login").performClick()
         Intents.intended(hasComponent(DashboardActivity::class.java.name))
     }
 
+    // Test for School login
     @Test
-    fun testEmptyFields_showsError() {
+    fun loginSchool_navigatesToSchoolDashboard() {
+        composeRule.onNodeWithTag("email").performTextInput("school@test.com")
+        composeRule.onNodeWithTag("password").performTextInput("school123")
         composeRule.onNodeWithTag("login").performClick()
-
-        composeRule.onNodeWithTag("errorText")
-            .assertIsDisplayed()
+        Intents.intended(hasComponent(SchoolDashboard::class.java.name))
     }
 
+    // Test for Admin login
     @Test
-    fun testInvalidEmail_showsError() {
-        composeRule.onNodeWithTag("email").performTextInput("invalidemail")
-        composeRule.onNodeWithTag("password").performTextInput("123456")
+    fun loginAdmin_navigatesToAdminDashboard() {
+        composeRule.onNodeWithTag("email").performTextInput("admin@test.com")
+        composeRule.onNodeWithTag("password").performTextInput("admin123")
         composeRule.onNodeWithTag("login").performClick()
-
-        composeRule.onNodeWithText("Please enter a valid email")
-            .assertIsDisplayed()
+        Intents.intended(hasComponent(AdminDashboardActivity::class.java.name))
     }
-
-
-
 }
